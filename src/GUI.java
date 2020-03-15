69a916bcd96a79ef24a7afd53b4e8b3bf2ef209b
Dudás András <raiandylightning@gmail.com>
2020-03-15 17:46:05 +0100

    Working!!!

--- src/GUI.java
+++ src/GUI.java
@@ -1,15 +1,21 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 import java.awt.*;
 
+
 public class GUI extends JFrame {
     boolean Multi;
-    int spacing=5;
+    int spacing = 5;
+    BufferedImage red, orange, yellow, green, lblue, dblue, purple;
 
     int[][] playerZone = new int[24][10];
 
-    public GUI() {
+    public GUI() throws IOException {
         this.setTitle("Tetris");
         this.setSize(650, 754);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,17 +31,24 @@ public class GUI extends JFrame {
         Click click = new Click();
         this.addMouseListener(click);
 
+        green = ImageIO.read(new File("img/Green.png"));
 
     }
 
+
     public class Board extends JPanel {
+
+
         public void paintComponent(Graphics g) {
-            g.setColor(Color.LIGHT_GRAY);
+            g.setColor(Color.lightGray);
             g.fillRect(0,0,640,720);
             g.setColor(Color.DARK_GRAY);
-            for (int i=0; i<10; i++)
-                for (int j=0; j<24;j++)
-                    g.fillRect(15+i*(spacing+20), j*(spacing+20)+105, 20, 20);
+            for (int i=0; i<10; i++) {
+                for (int j = 0; j < 24; j++) {
+                    g.fillRect(15 + i * (spacing + 20), j * (spacing + 20) + 105, 20, 20);
+                }
+            }
+            g.drawImage(green, 0, 0, this);
         }
     }
     public class Move implements MouseMotionListener {
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
+import java.io.IOException;
 
 public class Main implements Runnable{
 
     GUI gui = new GUI();
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         new Thread(new Main()).start();
 
     }
     @Override
     public void run(){
         gui.repaint();
+    }
+
+
+    public Main() throws IOException {
     }
 }
